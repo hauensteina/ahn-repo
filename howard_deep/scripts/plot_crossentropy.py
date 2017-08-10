df3b741445acfc4b3d43975b9f5ca61d6eff476d
Andreas Hauenstein <hauensteina@gmail.com>
2017-08-10 08:51:35 -0700

    plot_crossentropy.py aspect ratio

--- howard_deep/scripts/plot_crossentropy.py
+++ howard_deep/scripts/plot_crossentropy.py
@@ -12,7 +12,7 @@ def crossentropy(p,q):
  return -1 * (p * log(q) + (1.0-p) * log(1.0-q))
 
 #-----------------------------------------------------
-def contourplot(fig, ax,title,Z,xmin,xmax,ymin,ymax):
+def contourplot(fig,ax,title,Z,xmin,xmax,ymin,ymax):
     im = ax.imshow(Z,
                    cmap=plt.cm.Greys,
                    origin='lower',
@@ -27,7 +27,7 @@ def contourplot(fig, ax,title,Z,xmin,xmax,ymin,ymax):
     )
     ax.clabel(cset, inline=True, fmt='%1.1f', fontsize=10)
     box = ax.get_position()
-    cax = fig.add_axes([box.x0, box.y0, box.width, box.height/20])
+    cax = fig.add_axes([box.x0, box.y0+0.15, box.width, box.height/20])
 
     #im = ax.imshow(data, cmap='gist_earth')
     fig.colorbar(im, cax=cax, orientation='horizontal')
@@ -35,7 +35,7 @@ def contourplot(fig, ax,title,Z,xmin,xmax,ymin,ymax):
     ax.set_title(title)
 
 #--------------------------------------------------------
-def lineplot(ax, title,func,pmin,pmax,qmin,qmax,dp,dq):
+def lineplot(fig,ax,title,func,pmin,pmax,qmin,qmax,dp,dq):
     p = np.arange(pmin,pmax,dp)
     q = np.arange(qmin,qmax,dq)
 
@@ -51,9 +51,34 @@ def lineplot(ax, title,func,pmin,pmax,qmin,qmax,dp,dq):
     # Put a legend to the right of the current axis
     #ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
+#--------------------------------------------------------
+def wireplot(ax,title,func,pmin,pmax,qmin,qmax,dp,dq):
+    p = np.arange(pmin,pmax,dp)
+    q = np.arange(qmin,qmax,dq)
+
+    for qval in q:
+        ax.plot(p, func(p, qval),label = 'q=%.2f' % qval)
+    ax.set_title(title)
+    # Shrink current axis by 20%
+    #box = ax.get_position()
+    #ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    #fontP = FontProperties()
+    #fontP.set_size('small')
+    #ax.legend(loc='upper center', prop = fontP) # bbox_to_anchor=(1.5, 1.05))
+    # Put a legend to the right of the current axis
+    #ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
 #------------
 def main():
-    fig, axes = plt.subplots(nrows=1, ncols=2)
+    NCOLS=3
+    fig = plt.figure(figsize=(15,10))
+    contourax = fig.add_subplot(1,NCOLS,1)
+    contourax.set_aspect(1)
+    lineax = fig.add_subplot(1,NCOLS,2)
+    lineax.set_aspect(1)
+    wireax = fig.add_subplot(1,NCOLS,3)
+    wireax.set_aspect(1)
+
     pmin,pmax,qmin,qmax = 0.0, 1.0, 0.0, 1.0
     delta = 0.01
     p = np.arange(pmin+delta, pmax, delta)
@@ -63,8 +88,10 @@ def main():
     Z = crossentropy(P, Q)  # evaluation of the function on the grid
     # latex fashion title
     title = r'$z=-(p\/\log(q) + (1-p)\/\log(1-q))$'
-    contourplot(fig, axes[0], title, Z, pmin, pmax, qmin, qmax)
-    lineplot(axes[1], title, crossentropy, pmin+delta, pmax, 0.1, 1.0, delta, 0.1)
+    contourplot(fig, contourax, title, Z, pmin, pmax, qmin, qmax)
+    lineplot(fig, lineax, title, crossentropy, pmin+delta, pmax, 0.1, 1.0, delta, 0.1)
+    wireplot(wireax, title, crossentropy, pmin+delta, pmax, 0.1, 1.0, delta, 0.1)
     plt.show()
+
 if __name__ == '__main__':
     main()
