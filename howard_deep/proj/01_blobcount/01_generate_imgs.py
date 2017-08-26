3f91364063b7290820063a51770595b948b15629
hauensteina <hauensteina@gmail.com>
2017-08-26 21:48:57 +0000

    01_generate_imgs.py makes circles

--- howard_deep/proj/01_blobcount/01_generate_imgs.py
+++ howard_deep/proj/01_blobcount/01_generate_imgs.py
@@ -12,6 +12,8 @@
 from __future__ import division, print_function
 from pdb import set_trace as BP
 import os,sys,re,json
+import numpy as np
+from numpy.random import random
 import argparse
 import matplotlib as mpl
 mpl.use('Agg') # This makes matplotlib work without a display
@@ -49,23 +51,51 @@ def main():
     parser.add_argument( "--ntrain",   required=True, type=int)
     parser.add_argument( "--nval",     required=True, type=int)
     args=parser.parse_args()
-    gen_image(args.res,1,'train','tt')
+    #np.random.seed(0) # Make things reproducible
+    gen_images(args.ntrain, args.res, 1, 2, 'train')
 
-# Generate one image of resolution res with n circles in it.
-# Image goes to folder/fname.jpg, labels go to folder/fname.json.
+# Generate one image of resolution resxres with nblobs circles in it.
+# Image goes to folder/fname
 #----------------------------------------
-def gen_image(res,nblobs,folder,fname):
+def gen_image(res,nblobs,ofname):
+    # Set up matplotlib
     dpi=100.0
-    #fig = plt.figure(figsize=(res/1000.0, res/1000.0), dpi=1000,frameon=False)
     fig = plt.figure(figsize=(res/dpi,res/dpi),dpi=dpi)
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
     fig.add_axes(ax)
-    circle = plt.Circle((0, 0), 1.0, color='r')
-    ax.add_artist(circle)
-    # A line
-    # ax.plot([0, 1], [0.5, 0.5], color='k', linestyle='-', linewidth=5)
-    plt.savefig('myfig.jpg')
+
+    centers_x = np.random.uniform(0,1,nblobs)
+    centers_y = np.random.uniform(0,1,nblobs)
+    r = np.random.uniform(0.1,0.2,nblobs)
+
+    for i in range(nblobs):
+        circle = plt.Circle((centers_x[i], centers_y[i]), r[i], color='k')
+        ax.add_artist(circle)
+    plt.savefig(ofname)
+    return (list(centers_x), list(centers_y), list(r))
+
+# Generate nb_imgs images with minblobs to maxblobs circles.
+# Also generate a json file for each, giving the number of circles.
+#-------------------------------------------------------------------
+def gen_images(nb_imgs,res,minblobs,maxblobs,folder):
+    #BP()
+    nblobs = minblobs + np.random.randint(maxblobs-minblobs+1, size=nb_imgs)
+    for i in range(nb_imgs):
+        fname = '%07d' % i
+        fjpg  = folder + '/' + fname + '.jpg'
+        fjson = folder + '/' + fname + '.json'
+        # Make jpeg
+        (x,y,r) = gen_image(res, nblobs[i], fjpg)
+        plt.close('all')
+        # Dump metadata
+        #meta = { 'centers_x':x, 'centers_y':y, 'radii':r }
+        meta = { 'nblobs':nblobs[i] }
+        #BP()
+        meta_json = json.dumps(meta) + '\n'
+        with open(fjson,'w') as f: f.write(meta_json)
+
+
 
 if __name__ == '__main__':
     main()
