375e078c1e970b8e1e267289d2c0359937cb69c7
Andreas Hauenstein <hauensteina@gmail.com>
2017-10-23 12:31:08 -0700

    sgfgrabber get_contours

--- opencv/proj/02_sgfgrabber_ios/sgfgrabber/sgfgrabber/GrabFuncs.mm
+++ opencv/proj/02_sgfgrabber_ios/sgfgrabber/sgfgrabber/GrabFuncs.mm
@@ -11,6 +11,12 @@
 #import "Common.h"
 #import "GrabFuncs.h"
 
+//using namespace std;
+//typedef std::vector vector;
+typedef std::vector<std::vector<cv::Point> > Contours;
+typedef std::vector<cv::Point> Contour;
+static cv::RNG rng(12345);
+
 @implementation GrabFuncs
 //=========================
 
@@ -59,7 +65,7 @@ double median( cv::Mat channel )
     }
     
     return med;
-}
+} // median()
 
 // Automatic edge detection without parameters
 //--------------------------------------------------------------------
@@ -71,7 +77,74 @@ void auto_canny( const cv::Mat &src, cv::Mat &dst, float sigma=0.33)
     cv::Canny( src, dst, lower, upper);
 }
 
-//---------------------------------------------------------------------
+//#-----------------------
+//def get_contours(img):
+//#img   = cv2.GaussianBlur( img, (7, 7), 0)
+//#img   = cv2.medianBlur( img, 7)
+//#img = cv2.bilateralFilter(img, 11, 17, 17)
+//#edges = cv2.Canny(img, 60, 200)
+//edges = ut.auto_canny(img)
+//# find contours in the edge map
+//im2, cnts, hierarchy  = cv2.findContours(edges, cv2.RETR_LIST,
+//                                         cv2.CHAIN_APPROX_SIMPLE)
+//#cnts = sorted( cnts, key=cv2.contourArea, reverse=True)
+//#ut.show_contours( img, cnts)
+//
+//# Keep if larger than 0.1% of image
+//img_area = img.shape[0] * img.shape[1]
+//cnts = [ c for c in cnts if  cv2.contourArea(c) / img_area > 0.001 ]
+//# Keep if reasonably not-wiggly
+//#cnts = [ c for c in cnts if   cv2.arcLength(c, closed=True) / len(c) > 2.0 ]
+//cnts = [ c for c in cnts if   len(c) < 100 or cv2.arcLength(c, closed=True) / len(c) > 2.0 ]
+//
+//return cnts
+
+////---------------------------------------------------------------------
+//bool compVecByLenAsc( vector<cv::Point> &c1,  vector<cv::Point> &c2)
+//{
+//    bool res = c1.size() < c2.size();
+//    return res;
+//}
+
+//---------------------------------------------
+Contours get_contours( const cv::Mat &img)
+{
+    Contours conts;
+    std::vector<cv::Vec4i> hierarchy;
+    // Edges
+    cv::Mat m;
+    auto_canny( img, m);
+    // Find contours
+    findContours( m, conts, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+
+    // Keep if larger than 0.1% of image
+//    float img_area = img.rows * img.cols;
+//    float lim = img_area * 0.001;
+//    Contours large_conts;
+//    std::copy_if( conts.begin(), conts.end(), std::back_inserter(large_conts),
+//                 [lim](Contour c){return cv::contourArea(c) > lim;} );
+
+    // Keep if reasonably not-wiggly
+//    Contours straight_conts;
+//    std::copy_if( conts.begin(), conts.end(), std::back_inserter(straight_conts),
+//                 [](Contour c){return (c.size() < 100) || (cv::arcLength(c, true) / (float)c.size() > 2.0);} );
+    
+    return conts;
+} // get_contours()
+
+//-------------------------------------------------------
+void draw_contours( const Contours cont, cv::Mat &dst)
+{
+    // Draw contours
+    for( int i = 0; i< cont.size(); i++ )
+    {
+        cv::Scalar color = cv::Scalar( rng.uniform(50, 255), rng.uniform(50,255), rng.uniform(50,255) );
+        drawContours( dst, cont, i, color, 2, 8);
+    }
+} // draw_contours()
+
+
+//-----------------------------------------
 - (UIImage *) findBoard:(UIImage *)img
 {
     // Convert UIImage to Mat
@@ -81,11 +154,11 @@ void auto_canny( const cv::Mat &src, cv::Mat &dst, float sigma=0.33)
     //cv::Mat small;
     resize( m, m, 500);
     // Grayscale
-    //cv::Mat gray;
     cv::cvtColor( m, m, cv::COLOR_BGR2GRAY);
-    // Edges
-    cv::Mat edges;
-    auto_canny( m, m);
+    // Contours
+    Contours contours = get_contours(m);
+    cv::Mat drawing = cv::Mat::zeros( m.size(), CV_8UC3 );
+    draw_contours( contours, drawing);
 
 //    // Draw on Mat
 //    cv::Point pt1( x, y);
@@ -96,7 +169,7 @@ void auto_canny( const cv::Mat &src, cv::Mat &dst, float sigma=0.33)
 //    cv::rectangle( m, pt1, pt2, cv::Scalar(r,g,b,255)); // int thickness=1, int lineType=8, int shift=0)¶
 //
     // Convert back to UIImage
-    UIImage *res = MatToUIImage( m);
+    UIImage *res = MatToUIImage( drawing);
     return res;
 } // drawRectOnImage()
 
