3d7cd0cf678e71ccb1ec47cf68088ac8a9151c5e
Andreas Hauenstein <hauensteina@gmail.com>
2017-11-15 11:43:58 -0800

    sgfgraber refactor backup

--- opencv/proj/02_sgfgrabber_ios/sgfgrabber/sgfgrabber/Common.mm
+++ opencv/proj/02_sgfgrabber_ios/sgfgrabber/sgfgrabber/Common.mm
@@ -14,11 +14,6 @@
 #import <UIKit/UIKit.h>
 #import "Common.h"
 
-cplx I(0.0, 1.0);
-
-//===============
-// String Stuff
-//===============
 
 // Replacement for annoying [NSString stringWithFormat ...
 //---------------------------------------------------------
@@ -52,30 +47,4 @@ NSString* getFullPath( NSString *fname)
     return filePath;
 }
 
-//======
-// Math
-//======
 
-//---------------------------------------------------
-void _fft(cplx buf[], cplx out[], int n, int step)
-{
-    if (step < n) {
-        _fft( out, buf, n, step * 2);
-        _fft( out + step, buf + step, n, step * 2);
-        
-        for (int i = 0; i < n; i += 2 * step) {
-            cplx t = exp( -I * PI * (cplx(i) / cplx(n))) * out[ i + step];
-            buf[ i / 2]     = out[i] + t;
-            buf[ (i + n)/2] = out[i] - t;
-        }
-    }
-}
-
-//---------------------------
-void fft(cplx buf[], int n)
-{
-    cplx out[n];
-    for (int i = 0; i < n; i++) out[i] = buf[i];
-    
-    _fft( buf, out, n, 1);
-}
---
+++ opencv/proj/02_sgfgrabber_ios/sgfgrabber/sgfgrabber/Common_cpp.mm
@@ -0,0 +1,44 @@
+//
+//  Common.cpp
+//  sgfgrabber
+//
+//  Created by Andreas Hauenstein on 2017-11-15.
+//  Copyright © 2017 AHN. All rights reserved.
+//
+
+//======================================
+// Generally useful convenience funcs
+//======================================
+
+#include "Common_cpp.h"
+
+cplx I(0.0, 1.0);
+
+//======
+// Math
+//======
+
+//---------------------------------------------------
+void _fft(cplx buf[], cplx out[], int n, int step)
+{
+    if (step < n) {
+        _fft( out, buf, n, step * 2);
+        _fft( out + step, buf + step, n, step * 2);
+        
+        for (int i = 0; i < n; i += 2 * step) {
+            cplx t = exp( -I * PI * (cplx(i) / cplx(n))) * out[ i + step];
+            buf[ i / 2]     = out[i] + t;
+            buf[ (i + n)/2] = out[i] - t;
+        }
+    }
+}
+
+//---------------------------
+void fft(cplx buf[], int n)
+{
+    cplx out[n];
+    for (int i = 0; i < n; i++) out[i] = buf[i];
+    
+    _fft( buf, out, n, 1);
+}
+
