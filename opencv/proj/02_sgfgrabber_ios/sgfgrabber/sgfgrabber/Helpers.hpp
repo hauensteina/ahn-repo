786cf4125f12b900aae4dffd3b6c24a1526971ce
Andreas Hauenstein <hauensteina@gmail.com>
2018-01-27 11:16:46 -0800

    sgfgrabber cleanup

--- opencv/proj/02_sgfgrabber_ios/sgfgrabber/sgfgrabber/Helpers.hpp
+++ opencv/proj/02_sgfgrabber_ios/sgfgrabber/sgfgrabber/Helpers.hpp
@@ -18,6 +18,7 @@
 #include <regex>
 
 #include "Common.hpp"
+#include "Clust1D.hpp"
 
 // Apply inverse thresh and dilate grayscale image.
 //-------------------------------------------------------------------------------------------
@@ -230,6 +231,183 @@ inline void draw_sgf( const std::string &sgf_, cv::Mat &dst, int width)
         }
     } // ISLOOP
 } // draw_sgf()
+
+// Reject board if opposing lines not parallel
+// or adjacent lines not at right angles
+//-----------------------------------------------------------
+inline bool board_valid( Points2f board, const cv::Mat &img)
+{
+    double screenArea = img.rows * img.cols;
+    if (board.size() != 4) return false;
+    double area = cv::contourArea(board);
+    if (area / screenArea > 0.95) return false;
+    if (area / screenArea < 0.20) return false;
+    
+    double par_ang1   = (180.0 / M_PI) * angle_between_lines( board[0], board[1], board[3], board[2]);
+    double par_ang2   = (180.0 / M_PI) * angle_between_lines( board[0], board[3], board[1], board[2]);
+    double right_ang1 = (180.0 / M_PI) * angle_between_lines( board[0], board[1], board[1], board[2]);
+    double right_ang2 = (180.0 / M_PI) * angle_between_lines( board[0], board[3], board[3], board[2]);
+    //double horiz_ang   = (180.0 / M_PI) * angle_between_lines( board[0], board[1], cv::Point(0,0), cv::Point(1,0));
+    //NSLog(@"%f.2, %f.2, %f.2, %f.2", par_ang1,  par_ang2,  right_ang1,  right_ang2 );
+    //if (abs(horiz_ang) > 20) return false;
+    if (abs(par_ang1) > 20) return false;
+    if (abs(par_ang2) > 30) return false;
+    if (abs(right_ang1 - 90) > 20) return false;
+    if (abs(right_ang2 - 90) > 20) return false;
+    return true;
+} // board_valid()
+
+// Replace close clusters of vert lines by their average.
+//-----------------------------------------------------------------------------------
+inline void dedup_verticals( std::vector<cv::Vec2f> &lines, const cv::Mat &img)
+{
+    if (SZ(lines) < 3) return;
+    // Cluster by x in the middle
+    //const double wwidth = 32.0;
+    const double wwidth = 8.0;
+    const double middle_y = img.rows / 2.0;
+    const int min_clust_size = 0;
+    auto Getter =  [middle_y](cv::Vec2f line) { return x_from_y( middle_y, line); };
+    auto vert_line_cuts = Clust1D::cluster( lines, wwidth, Getter);
+    std::vector<std::vector<cv::Vec2f> > clusters;
+    Clust1D::classify( lines, vert_line_cuts, min_clust_size, Getter, clusters);
+    
+    // Average the clusters into single lines
+    lines.clear();
+    ISLOOP (clusters) {
+        auto &clust = clusters[i];
+        double theta = vec_avg( clust, [](cv::Vec2f line){ return line[1]; });
+        double rho   = vec_avg( clust, [](cv::Vec2f line){ return line[0]; });
+        cv::Vec2f line( rho, theta);
+        lines.push_back( line);
+    }
+} // dedup_verticals()
+
+// Replace close clusters of horiz lines by their average.
+//-----------------------------------------------------------------------------------
+inline void dedup_horizontals( std::vector<cv::Vec2f> &lines, const cv::Mat &img)
+{
+    if (SZ(lines) < 3) return;
+    // Cluster by y in the middle
+    const double wwidth = 32.0;
+    const double middle_x = img.cols / 2.0;
+    const int min_clust_size = 0;
+    auto Getter =  [middle_x](cv::Vec2f line) { return y_from_x( middle_x, line); };
+    auto horiz_line_cuts = Clust1D::cluster( lines, wwidth, Getter);
+    std::vector<std::vector<cv::Vec2f> > clusters;
+    Clust1D::classify( lines, horiz_line_cuts, min_clust_size, Getter, clusters);
+    
+    // Average the clusters into single lines
+    lines.clear();
+    ISLOOP (clusters) {
+        auto &clust = clusters[i];
+        double theta = vec_avg( clust, [](cv::Vec2f line){ return line[1]; });
+        double rho   = vec_avg( clust, [](cv::Vec2f line){ return line[0]; });
+        cv::Vec2f line( rho, theta);
+        lines.push_back( line);
+    }
+} // dedup_horizontals()
+
+// Find a line close to the middle with roughly median theta.
+// The lines should be sorted by rho.
+//--------------------------------------------------------------------------
+inline int good_center_line( const std::vector<cv::Vec2f> &lines)
+{
+    const int r = 2;
+    //const double EPS = 4 * PI/180;
+    auto thetas = vec_extract( lines, [](cv::Vec2f line) { return line[1]; } );
+    auto med_theta = vec_median( thetas);
+    
+    // Find a line close to the middle where theta is close to median theta
+    int half = SZ(lines)/2;
+    double mind = 1E9;
+    int minidx = -1;
+    ILOOP (r+1) {
+        if (half - i >= 0) {
+            double d = fabs( med_theta - thetas[half-i]);
+            if (d < mind) {
+                mind = d;
+                minidx = half - i;
+            }
+        }
+        if (half + i < SZ(lines)) {
+            double d = fabs( med_theta - thetas[half+i]);
+            if (d < mind) {
+                mind = d;
+                minidx = half + i;
+            }
+        }
+    } // ILOOP
+    return minidx;
+} // good_center_line()
+
+// Adjacent lines should have similar slope
+//-----------------------------------------------------------------------------
+inline void filter_vert_lines( std::vector<cv::Vec2f> &vlines)
+{
+    const double eps = 10.0;
+    std::sort( vlines.begin(), vlines.end(), [](cv::Vec2f &a, cv::Vec2f &b) { return a[0] < b[0]; });
+    int med_idx = good_center_line( vlines);
+    if (med_idx < 0) return;
+    const double med_theta = vlines[med_idx][1];
+    // Going left and right, theta should not change abruptly
+    std::vector<cv::Vec2f> good;
+    good.push_back( vlines[med_idx]);
+    const double EPS = eps * PI/180;
+    double prev_theta;
+    // right
+    prev_theta = med_theta;
+    for (int i = med_idx+1; i < SZ(vlines); i++ ) {
+        double d = fabs( vlines[i][1] - prev_theta) + fabs( vlines[i][1] - med_theta);
+        if (d < EPS) {
+            good.push_back( vlines[i]);
+            prev_theta = vlines[i][1];
+        }
+    }
+    // left
+    prev_theta = med_theta;
+    for (int i = med_idx-1; i >= 0; i-- ) {
+        double d = fabs( vlines[i][1] - prev_theta) + fabs( vlines[i][1] - med_theta);
+        if (d < EPS) {
+            good.push_back( vlines[i]);
+            prev_theta = vlines[i][1];
+        }
+    }
+    vlines = good;
+} // filter_vert_lines()
+
+// Adjacent lines should have similar slope
+//-----------------------------------------------------------------
+inline void filter_horiz_lines( std::vector<cv::Vec2f> &hlines)
+{
+    const double eps = 1.1;
+    std::sort( hlines.begin(), hlines.end(), [](cv::Vec2f &a, cv::Vec2f &b) { return a[0] < b[0]; });
+    int med_idx = good_center_line( hlines);
+    if (med_idx < 0) return;
+    double theta = hlines[med_idx][1];
+    // Going up and down, theta should not change abruptly
+    std::vector<cv::Vec2f> good;
+    good.push_back( hlines[med_idx]);
+    const double EPS = eps * PI/180;
+    double prev_theta;
+    // down
+    prev_theta = theta;
+    for (int i = med_idx+1; i < SZ(hlines); i++ ) {
+        if (fabs( hlines[i][1] - prev_theta) < EPS) {
+            good.push_back( hlines[i]);
+            prev_theta = hlines[i][1];
+        }
+    }
+    // up
+    prev_theta = theta;
+    for (int i = med_idx-1; i >= 0; i-- ) {
+        if (fabs( hlines[i][1] - prev_theta) < EPS) {
+            good.push_back( hlines[i]);
+            prev_theta = hlines[i][1];
+        }
+    }
+    hlines = good;
+} // filter_horiz_lines()
 
 #endif /* __clusplus */
 #endif /* Helpers_hpp */
