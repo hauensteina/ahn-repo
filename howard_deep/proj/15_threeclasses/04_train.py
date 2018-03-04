28395344f1aeb2a8cb8842caaed6ee65b3e17c9f
hauensteina <hauensteina@gmail.com>
2018-03-04 18:56:36 +0000

    15_threeclasses coreml export uses multiarray input

--- howard_deep/proj/15_threeclasses/04_train.py
+++ howard_deep/proj/15_threeclasses/04_train.py
@@ -12,7 +12,7 @@
 from __future__ import division, print_function
 from pdb import set_trace as BP
 import inspect
-import os,sys,re,json
+import os,sys,re,json, shutil
 import numpy as np
 from numpy.random import random
 import argparse
@@ -160,16 +160,13 @@ def main():
     parser.add_argument( "--rate", required=True, type=float)
     args = parser.parse_args()
     model = BEWModel( args.resolution, args.rate)
+    wfname =  'nn_bew.weights'
+    if os.path.exists( wfname):
+        model.model.load_weights( wfname)
     images = ut.get_data( SCRIPTPATH, (args.resolution,args.resolution), color_mode='rgb')
     meta   = get_meta_from_fnames( SCRIPTPATH)
-    # Normalize training and validation data by train data mean and std
-    means,stds = ut.get_means_and_stds(images['train_data'])
-    # ut.normalize( images['train_data'], means, stds)
-    # ut.normalize( images['valid_data'], means, stds)
     ut.dumb_normalize( images['train_data'])
     ut.dumb_normalize( images['valid_data'])
-
-    # ut.dsi( images['valid_data'][0], 'dsi.jpg')
 
     model.model.fit(images['train_data'], meta['train_classes_hot'],
                     batch_size=BATCH_SIZE, epochs=args.epochs,
@@ -177,37 +174,26 @@ def main():
     ut.dump_n_best_and_worst( 10, model.model, images, meta, 'train')
     ut.dump_n_best_and_worst( 10, model.model, images, meta, 'valid')
 
-    # Inspect best and worst on train, test, or valid
-
-    #print(preds)
-    # print('>>>>>iter %d' % i)
-    # for idx,layer in enumerate(model.model.layers):
-    #     weights = layer.get_weights() # list of numpy arrays
-    #     print('Weights for layer %d:',idx)
-    #     print(weights)
-    #model.model.fit(images['train_data'], meta['train_classes'],
-    #                batch_size=BATCH_SIZE, epochs=args.epochs)
-    model.model.save('nn_bew.hd5')
-    model.model.save_weights('nn_bew.weights')
+    shutil.move( wfname, wfname + '.bak')
+    model.model.save( 'nn_bew.hd5')
+    model.model.save_weights( wfname)
 
     coreml_model = coremltools.converters.keras.convert( model.model,
-                                                         input_names=['image'],
-                                                         image_input_names='image',
+                                                         #input_names=['image'],
+                                                         #image_input_names='image',
                                                          class_labels = ['b', 'e', 'w'],
-                                                         predicted_feature_name='bew',
-                                                         image_scale = 1/128.0,
-                                                         red_bias = -1,
-                                                         green_bias = -1,
-                                                         blue_bias = -1);
+                                                         predicted_feature_name='bew'
+                                                         #image_scale = 1/128.0,
+                                                         #red_bias = -1,
+                                                         #green_bias = -1,
+                                                         #blue_bias = -1
+    );
 
     coreml_model.author = 'ahn'
     coreml_model.license = 'MIT'
     coreml_model.short_description = 'Classify go stones and intersections'
-    coreml_model.input_description['image'] = 'A 23x23 pixel Image'
+    #coreml_model.input_description['image'] = 'A 23x23 pixel Image'
     coreml_model.output_description['output1'] = 'A one-hot vector for classes black empty white'
-    #coreml_model.save('keras_mnist_cnn.mlmodel')
-
-    #coreml_model = coremltools.converters.keras.convert( model.model, input_names=['image'], image_input_names='image')
     coreml_model.save("nn_bew.mlmodel")
 
 if __name__ == '__main__':
