39375163bf49ac10272b1d79e71e075fe9abbee2
hauensteina <hauensteina@gmail.com>
2018-03-05 05:29:20 +0000

    15_threeclasses GPU/CPU switch

--- howard_deep/proj/15_threeclasses/04_train.py
+++ howard_deep/proj/15_threeclasses/04_train.py
@@ -31,7 +31,27 @@ sys.path.append(re.sub(r'/proj/.*',r'/pylib', SCRIPTPATH))
 import ahnutil as ut
 
 
-BATCH_SIZE=32
+import tensorflow as tf
+from keras import backend as K
+
+num_cores = 4
+
+GPU=1
+if GPU:
+    num_GPU = 1
+    num_CPU = 1
+else:
+    num_CPU = 1
+    num_GPU = 0
+
+config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,\
+        inter_op_parallelism_threads=num_cores, allow_soft_placement=True,\
+        device_count = {'CPU' : num_CPU, 'GPU' : num_GPU})
+session = tf.Session(config=config)
+K.set_session(session)
+
+
+BATCH_SIZE=128
 
 #---------------------------
 def usage(printmsg=False):
