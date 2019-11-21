17f097664d5c785724a3f266de5a036f31a5bb7e
andreas.hauenstein@protxx.com <hauensteina@gmail.com>
2019-11-21 11:38:25 -0800

    mastergo-leela

---
+++ mastergo-leela/01_split_all_match.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+# Split all_sgf.sgf into separate sgf files for mastergo import.
+# AHN, Nov 2019
+
+import os,sys,shutil
+import argparse
+from pdb import set_trace as BP
+
+OUTFOLDER = 'split_sgfs'
+MINDATE = '2018-08-01'
+FILES_PER_FOLDER = 100 * 1000
+
+#--------------
+def usage():
+    name = os.path.basename( __file__)
+    msg = '''
+
+    Description:
+      %s: Split a multi-sgf file into separate sgf files for mastergo import
+    Example:
+      %s --file all_match.sgf
+    Output goes to folders %s/n, where n increases every %d games.
+
+--
+''' % (name, name, OUTFOLDER, FILES_PER_FOLDER)
+
+    return msg
+
+#------------
+def main():
+    parser = argparse.ArgumentParser( usage=usage())
+    parser.add_argument( "--file", required=True)
+    args = parser.parse_args()
+
+    if os.path.exists( OUTFOLDER):
+        print( '%s exists. Please move it away.' % OUTFOLDER)
+        exit(1)
+
+    os.mkdir( OUTFOLDER)
+    run( args.file)
+
+#-------------------------
+def run( infname):
+    sgf = ''
+    sgfcount = 0
+    foldercount = 0
+
+    print( 'patience please, chugging through old games...')
+    with open( infname) as inf:
+        for line in inf:
+            line = line.strip()
+            if line.startswith( '(;GM'):
+                if sgf:
+                    ofname, dt = getfname( sgf)
+                    if ofname and (dt >= MINDATE):
+                        if not sgfcount % FILES_PER_FOLDER:
+                            foldercount += 1
+                            outfolder = '%s/%0.3d' % (OUTFOLDER, foldercount)
+                            os.mkdir( outfolder)
+                        with open( outfolder + '/' + ofname, 'w') as of:
+                            print( ofname)
+                            of.write( sgf)
+                            sgfcount += 1
+                    sgf = ''
+
+            sgf += line + '\n'
+
+# Return a god outfile name for an sgf game record.
+# Also returns the date played.
+#----------------------------------------------------
+def getfname( sgf):
+    getfname.gamenum += 1
+    tstr = sgf.split( 'DT[')[1].strip()
+    dt = tstr.split( ']')[0].strip()
+    fname = '%0.10d-%s.sgf' % (getfname.gamenum, dt)
+    return fname, dt
+getfname.gamenum = 0
+
+if __name__ == '__main__':
+    main()
---
+++ mastergo-leela/readme.txt
@@ -0,0 +1,22 @@
+mastergo-leela
+===============
+AHN, Nov 2019
+
+Scripts to process the leela zero match game archive into a folder
+of sgf files wivch can be imported into mastergo.
+
+The leela zero match archive is here:
+
+https://leela.online-go.com/zero/all_match.sgf.xz
+
+To uncompress, use
+
+$ unxz all_match.sgf.xz
+
+The archive is from July 2019. I hope they update this at some point.
+
+A copy of the archive is at
+
+s3://ahn-uploads/all_match.sgf.xz
+
+=== The End ===
