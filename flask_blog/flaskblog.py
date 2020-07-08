7c0a03bcc994c8e1ec4e9f4bc3c8b5cdd68e3ae8
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-07-07 18:16:34 -0700

    flask_blog register form

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,7 +1,10 @@
 
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '769d3f053b8f29f33041d5b7fda804b3'
 
 posts = [
     {
@@ -26,6 +29,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template( 'about.tmpl')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template( 'register.tmpl', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template( 'login.tmpl', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run( debug=True)
