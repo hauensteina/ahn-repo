2ef99bfc17513ce997d3fcff76895a5ccc2941d5
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-11-11 14:02:39 -0800

    tile_2d.py

--- pyth/proj/13_tiling/tile_2d.py
+++ pyth/proj/13_tiling/tile_2d.py
@@ -4,94 +4,180 @@
 from pdb import set_trace as BP
 import numpy as np
 
-g_size = 3 # size x size grid
-g_marg = g_size-1 # Add some margin
-g_totsize = g_size + 2 * g_marg # pad both sides
-
 g_pieces = [
-    np.array([
-        [1,1]
-    ]),
-    np.array([
-        [1,1,1]
-    ]),
-    np.array([
-        [0,0,1],
-        [1,1,1]
-    ])
+    # easy 3x3
+    [
+        np.array([
+            [1,1]
+        ]),
+        np.array([
+            [1,1,1]
+        ]),
+        np.array([
+            [0,0,1],
+            [1,1,1]
+        ])
+    ],
+    # 8x8
+    [
+        np.array([
+            [1,1,1,1,1]
+        ]),
+        np.array([
+            [1,1,1],
+            [0,1,1]
+        ]),
+        np.array([
+            [1,1,0,0],
+            [0,1,1,1]
+        ]),
+        np.array([
+            [1,1,1,1],
+            [0,0,1,0]
+        ]),
+        np.array([
+            [1,1,1],
+            [0,1,0],
+            [0,1,0]
+        ]),
+        np.array([
+            [1,0,0,0],
+            [1,1,1,1]
+        ]),
+        np.array([
+            [0,0,1,1],
+            [1,1,1,0]
+        ]),
+        np.array([
+            [1,1],
+            [1,0],
+            [1,1]
+        ]),
+        np.array([
+            [0,1,1],
+            [0,0,1],
+            [1,1,1]
+        ]),
+        np.array([
+            [0,0,1],
+            [1,1,1],
+            [1,0,0],
+            [1,0,0]
+        ]),
+        np.array([
+            [1],
+            [1],
+            [1]
+        ]),
+        np.array([
+            [0,0,1,0],
+            [1,1,1,1]
+        ]),
+        np.array([
+            [1,1,1,1]
+        ])
+    ]
 ]
 
-g_pieces = [p * (idx + 1) for idx,p in enumerate( g_pieces)]
+# Turn the ones into piece number
+for i,pieces in enumerate( g_pieces):
+    g_pieces[i] = [p * (idx + 1) for idx,p in enumerate( pieces)]
 
-def main():
-    grid = np.zeros( (g_totsize, g_totsize))
-    res = solve( grid, g_pieces)
-    #print( res)
-    print( 'Inner loops: %d' % solve.loopcount)
-    print( 'Found a total of %d solutions.' % solve.nsolutions)
-    for s in solve.solutions:
-        display_solution(s)
+#===================
+class Solution:
+    ' A solution is a hashable grid '
+    def __init__( self, grid):
+        self.grid = grid
 
-def solve( grid, pieces, depth=0):
-    ' Tile the grid with the pieces '
-    if len(pieces) == 0:
-        sol_hash = hash_solution( grid)
-        if sol_hash not in solve.solution_hashes:
-            solve.solution_hashes.add( sol_hash)
-            solve.solutions.append( grid)
-            solve.nsolutions += 1
-            #print( '>>>>>>>>> New solution found after %d loops' % solve.loopcount)
-        return 'success'
-    for pidx, p in enumerate( pieces):
-        for rp in rotations( p):
-            for row in range( g_size - rp.shape[0] + 1):
-                for col in range( g_size - rp.shape[1] + 1):
-                    solve.loopcount += 1
-                    g = grid.copy()
-                    r,c = totrc( row, col)
-                    add_window( g, rp, r, c)
-                    if islegal(g, pidx+depth+1):
-                        #print( 'placed piece %d at %d,%d' % (pidx+depth+1, row, col))
-                        remaining_pieces = pieces[1:]
-                        solve( g, remaining_pieces, depth+1)
-                    else:
-                        #print( 'piece %d illegal at %d,%d' % (pidx+depth+1, row, col))
-                        pass
+    def __hash__(self):
+        ' Hash and take out symmetries '
+        maxhash = None
+        for rs in rotations( self.grid):
+            h = hash( repr(rs))
+            if maxhash is None or maxhash < h:
+                maxhash = h
+        return maxhash
 
-        break # if one piece cannot be placed, no use trying other
-    #print( 'dead end')
-    return 'failure'
-solve.loopcount = 0
-solve.nsolutions = 0
-solve.solutions = []
-solve.solution_hashes = set()
+    def __eq__(self, other):
+        ' Compare and take out symmetries '
+        rots = rotations( self.grid)
+        for rs in rots:
+            if repr(rs) == repr(other.grid):
+                return True
+        return False
 
-def display_solution(s):
-    ' Show solution on screen '
-    print()
-    for row in range(g_size):
-        for col in range(g_size):
-            r,c = totrc( row, col)
-            print( '%d' % s[r,c], end='')
+    def size( self):
+        ' Grid size without n-1 padding '
+        return int( (self.grid.shape[0] + 2) / 3)
+
+    def pr( self):
         print()
+        for row in range( self.size()):
+            for col in range( self.size()):
+                r,c = padrc( self.size(), row, col)
+                print( '%d' % self.grid[r,c], end='')
+            print()
 
-def hash_solution(s):
-    ' Hash a solution, eliminating symmetries '
-    maxhash = None
-    for rs in rotations( s):
-        h = hash( repr(rs))
-        if maxhash is None or maxhash < h:
-            maxhash = h
-    return maxhash
+#-------------
+def main():
+    pieces = g_pieces[1]
+    size2 = sum( [np.sum( np.sign(p)) for p in pieces])
+    size = int(np.sqrt( size2))
+    pad = size-1
+    padsize = size + 2 * pad # pad both sides
+    grid = np.zeros( (padsize, padsize))
+    solve( size, grid, pieces)
+    print( 'Inner loops: %d' % solve.loopcount)
+    print( 'Found a total of %d solutions.' % len(solve.solutions))
+    for s in solve.solutions:
+        s.pr()
 
-def totrc( r, c):
-    ' Shift row, col into totsize grid with margin '
-    return r + g_marg, c + g_marg
+#-------------------------------------------
+def solve( size, grid, pieces, depth=0):
+    ' Tile the grid with the pieces '
+    try:
+        if len(pieces) == 0:
+            #BP()
+            solve.solutions.add( Solution(grid))
+            print( '>>>>>>>>> New solution found after %d loops' % solve.loopcount)
+            return 'success'
+        for pidx, p in enumerate( pieces):
+            for rp in rotations( p):
+                for row in range( size - rp.shape[0] + 1):
+                    for col in range( size - rp.shape[1] + 1):
+                        solve.loopcount += 1
+                        g = grid.copy()
+                        r,c = padrc( size, row, col)
+                        add_window( g, rp, r, c)
+                        if islegal( size, g, pidx+depth+1):
+                            #print( 'placed piece %d at %d,%d' % (pidx+depth+1, row, col))
+                            remaining_pieces = pieces[1:]
+                            solve( size, g, remaining_pieces, depth+1)
+                        else:
+                            #print( 'piece %d illegal at %d,%d' % (pidx+depth+1, row, col))
+                            pass
 
+            break # if one piece cannot be placed, no use trying other
+        #print( 'dead end')
+        return 'failure'
+    except Exception as e:
+        BP()
+        tt=42
+
+solve.loopcount = 0
+solve.solutions = set()
+
+#-------------------------
+def padrc( size, r, c):
+    ' Shift row, col into padded grid '
+    return r + size-1, c + size-1
+
+#------------------------------------
 def add_window( arr, window, r, c):
     ' Add window to arr at position r,c for left upper corner of win in arr '
     arr[ r:r+window.shape[0], c:c+window.shape[1] ] += window
 
+#-------------------------
 def rotations( grid):
     ' Return a list with all 8 rotations/mirrors of a grid '
     h = grid.shape[0]
@@ -110,21 +196,25 @@ def rotations( grid):
         grid = rot( grid)
     return res
 
-def islegal( g, cur_piece_num):
+#---------------------------------------
+def islegal( size, g, cur_piece_num):
     '''
     A grid is legal if the inner sum equals the total sum (no protrusions)
     and the maximum is less than the current piece number (no overlap)
     '''
+    pad = size - 1
     ssum = np.sum(g)
-    innersum = np.sum( g[ g_marg : g_marg + g_size, g_marg : g_marg + g_size ])
+    innersum = np.sum( g[ pad : pad + size, pad : pad + size ])
     if ssum != innersum: return False
     if np.max(g) > cur_piece_num: return False
     return True
 
+#-------------------------
 def rot( grid):
     ' Rotate a 2d grid clockwise '
     return np.rot90( grid,1,(1,0))
 
+#-------------------------
 def mirror( grid):
     ' Mirrors a 2d grid left to right'
     return np.flip( grid,1)
