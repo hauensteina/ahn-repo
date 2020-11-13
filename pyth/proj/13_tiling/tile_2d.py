ecd8471ef1eb2dd4abd755944d89a6b4988c9747
Andreas Hauenstein <hauensteina@gmail.com>
2020-11-13 00:04:24 +0000

    add more sizes to tile_2d.py

--- pyth/proj/13_tiling/tile_2d.py
+++ pyth/proj/13_tiling/tile_2d.py
@@ -3,9 +3,10 @@
 
 from pdb import set_trace as BP
 import numpy as np
+#import cupy as np
 
-g_pieces = [
-    # easy 3x3
+g_pieces = {
+    '3x3':
     [
         np.array([
             [1,1]
@@ -18,7 +19,123 @@ g_pieces = [
             [1,1,1]
         ])
     ],
-    # 8x8
+    '5x5':
+    [
+        np.array([
+            [1,1,1]
+        ]),
+        np.array([
+            [1,1],
+            [1,1]
+        ]),
+        np.array([
+            [1,1,1],
+            [0,1,1]
+        ]),
+        np.array([
+            [1,1],
+            [1,1],
+            [0,1]
+        ]),
+        np.array([
+            [1,0],
+            [1,0],
+            [1,1]
+        ]),
+        np.array([
+            [1,1,0],
+            [0,1,1]
+        ])
+    ],
+    '6x6':
+    [
+        np.array([
+            [1,1,1]
+        ]),
+        np.array([
+            [1,1,1],
+            [0,1,0]
+        ]),
+        np.array([
+            [1,1,1,1]
+        ]),
+        np.array([
+            [1,1,1],
+            [0,1,0],
+            [0,1,0]
+        ]),
+        np.array([
+            [0,0,1],
+            [1,1,1]
+        ]),
+        np.array([
+            [1,0,0],
+            [1,0,0],
+            [1,1,1]
+        ]),
+        np.array([
+            [1,1],
+            [1,1]
+        ]),
+        np.array([
+            [0,1,1],
+            [0,1,1],
+            [1,1,1]
+        ])
+    ],
+    '7x7':
+    [
+        np.array([
+            [1,1,1,1]
+        ]),
+        np.array([
+            [1,1,1],
+            [1,0,0]
+        ]),
+        np.array([
+            [0,1,1],
+            [1,1,1]
+        ]),
+        np.array([
+            [0,1],
+            [0,1],
+            [1,1]
+        ]),
+        np.array([
+            [0,1],
+            [1,1],
+            [0,1]
+        ]),
+        np.array([
+            [1,0,0],
+            [1,0,0],
+            [1,1,1]
+        ]),
+        np.array([
+            [1,1],
+            [1,1]
+        ]),
+        np.array([
+            [1,0,0,0],
+            [1,1,1,1]
+        ]),
+        np.array([
+            [0,1],
+            [0,1],
+            [1,1],
+            [0,1]
+        ]),
+        np.array([
+            [1,1,1,1,1]
+        ]),
+        np.array([
+            [1],
+            [1],
+            [1],
+            [1]
+        ])
+    ],
+    '8x8':
     [
         np.array([
             [1,1,1,1,1]
@@ -77,11 +194,11 @@ g_pieces = [
             [1,1,1,1]
         ])
     ]
-]
+} # g_pieces
 
 # Turn the ones into piece number
-for i,pieces in enumerate( g_pieces):
-    g_pieces[i] = [p * (idx + 1) for idx,p in enumerate( pieces)]
+for k in g_pieces.keys():
+    g_pieces[k] = [p * (idx + 1) for idx,p in enumerate( g_pieces[k])]
 
 #===================
 class Solution:
@@ -120,7 +237,7 @@ class Solution:
 
 #-------------
 def main():
-    pieces = g_pieces[1]
+    pieces = g_pieces['5x5']
     size2 = sum( [np.sum( np.sign(p)) for p in pieces])
     size = int(np.sqrt( size2))
     pad = size-1
@@ -132,37 +249,43 @@ def main():
     for s in solve.solutions:
         s.pr()
 
-#-------------------------------------------
-def solve( size, grid, pieces, depth=0):
+#---------------------------------------------------------
+def solve( size, grid, pieces, depth=0, row_=0, col_=0):
     ' Tile the grid with the pieces '
+    if depth == 0: solve.progress = np.zeros( len(pieces))
+    #if (depth == size) and (row_ + col_ == 0):
+        #print( 'depth: %d %s' % (depth, str(solve.progress)))
     try:
         if len(pieces) == 0:
             #BP()
+            #print( solve.progress)
             solve.solutions.add( Solution(grid))
+            #print( solve.progress)
             print( '>>>>>>>>> New solution found after %d loops' % solve.loopcount)
             return 'success'
-        for pidx, p in enumerate( pieces):
-            if depth == 0:
-                print( 'Piece %d/%d' % (pidx+1, len(pieces)))
-            rots = rotations( p)
-            for ridx,rp in enumerate( rots):
-                if depth == 0:
-                    print( 'Rotation %d/%d' % (ridx+1, len(rots)))
-                for row in range( size - rp.shape[0] + 1):
-                    for col in range( size - rp.shape[1] + 1):
-                        solve.loopcount += 1
-                        g = grid.copy()
-                        r,c = padrc( size, row, col)
-                        add_window( g, rp, r, c)
-                        if islegal( size, g, pidx+depth+1):
-                            #print( 'placed piece %d at %d,%d' % (pidx+depth+1, row, col))
-                            remaining_pieces = pieces[1:]
-                            solve( size, g, remaining_pieces, depth+1)
-                        else:
-                            #print( 'piece %d illegal at %d,%d' % (pidx+depth+1, row, col))
-                            pass
-
-            break # if one piece cannot be placed, no use trying other
+        p = pieces[0]
+        rots = rotations( p)
+        for ridx,rp in enumerate( rots):
+            solve.progress[depth] = ridx
+            for row in range( size - rp.shape[0] + 1):
+                for col in range( size - rp.shape[1] + 1):
+                    #if (row + col == 0) and (depth+1 == len(pieces)):
+                    #    print( 'xx ' + str(solve.progress))
+                    indent = '  ' * depth
+                    if depth < 2: #int(np.log(size*size)):
+                        print( '%spiece:%d rot:%d/%d row:%d col:%d' % (indent, depth+1, ridx+1, len(rots), row, col))
+                    solve.loopcount += 1
+                    g = grid.copy()
+                    r,c = padrc( size, row, col)
+                    add_window( g, rp, r, c)
+                    if islegal( size, g, depth+1):
+                        #print( 'placed piece %d at %d,%d' % (pidx+depth+1, row, col))
+                        #print( 'placing piece %d at %d %d with rotation %d' % (depth, row, col, ridx))
+                        remaining_pieces = pieces[1:]
+                        solve( size, g, remaining_pieces, depth+1, row, col)
+                    else:
+                        pass
+                        #print( 'piece %d illegal at %d,%d' % (depth+1, row, col))
         #print( 'dead end')
         return 'failure'
     except Exception as e:
@@ -171,6 +294,7 @@ def solve( size, grid, pieces, depth=0):
 
 solve.loopcount = 0
 solve.solutions = set()
+solve.progress = None
 
 #-------------------------
 def padrc( size, r, c):
