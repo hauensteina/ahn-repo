0dcee2be58741061265ba936c6e16dbc6772d382
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-12-07 12:20:28 -0800

    13_tiling viz3d.py cube viz finished

--- pyth/proj/13_tiling/viz3d.py
+++ pyth/proj/13_tiling/viz3d.py
@@ -165,9 +165,10 @@ def main():
 def visualize( pieces):
     w = 14.0
     h = 8.0
+    S = pieces[0].shape[0]
     space = 0.05
     marg = 0.05
-    cube_s = 0.9
+    cube_s = 0.7
     cmap = plt.get_cmap('Set1')
     fig = plt.figure( figsize=(w,h))
     assembled = [False] * len(pieces)
@@ -181,7 +182,7 @@ def visualize( pieces):
             return
         if not assembled[piece_idx]:
             color = cmap.colors[piece_idx]
-            artist_list = viz_piece( p, ax_cube, color)
+            artist_list = viz_piece_in_cube( p, ax_cube, color)
             assembled[piece_idx] = artist_list
         else:
             artist_list = assembled[piece_idx].values()
@@ -194,8 +195,13 @@ def visualize( pieces):
     fig.canvas.mpl_connect( 'pick_event', cb_click_piece)
 
     # The whole cube
-    ax_cube = fig.add_axes( [-marg * h/w, marg, cube_s * h/w, cube_s], projection='3d')
-    #ax_cube = fig.add_axes( [0, marg, cube_s * h/w, cube_s], projection='3d')
+    cube_x = marg * h/w
+    cube_width = cube_s * h/w
+    ax_cube = fig.add_axes( [cube_x, 3 * marg, cube_width, cube_s], projection='3d')
+    ax_cube.set_xlabel( "x")
+    ax_cube.set_ylabel( "y")
+    ax_cube.set_zlabel( "z")
+    ax_cube.grid( False)
     plt.axis( 'off')
     # A 4x4 grid for the individual pieces
     ax_pieces = []
@@ -207,7 +213,7 @@ def visualize( pieces):
         y = marg + r * rowheight
         if r: y += (r) * space
         for c in range( piece_grid_sz):
-            x = h/w * 0.8 * cube_s + c * colwidth
+            x = cube_x + cube_width + marg * h/w + c * colwidth # h/w * 0.8 * cube_s + c * colwidth
             if c: x += (c) * space * h/w
             # Setting picker arg to a tolerance margin enables pick event on axes
             ax = fig.add_axes( [x, y, colwidth, rowheight], projection='3d', picker=5)
@@ -226,31 +232,13 @@ def visualize( pieces):
     # btn_remove = Button( ax_remove, 'Remove')
     # btn_remove.on_clicked( cb_btn_remove)
 
-    #fig = plt.figure()
-    #ax = fig.gca( projection='3d')
-    ax_cube.set_aspect('auto')
-    # Manually fix aspect ratio. Uggh.
-    ax_cube.set_xlim3d(-0.5, 5.7)
-    ax_cube.set_ylim3d(-0.5, 5.7)
-    ax_cube.set_zlim3d(-0.5, 4.5)
-
-    #ax_cube.set_xlabel( "x")
-    #ax_cube.set_ylabel( "y")
-    #ax_cube.set_zlabel( "z")
-    ax_cube.grid( False)
-    for idx,p in enumerate(pieces):
-        color = cmap.colors[idx]
-        # The assembled cube
-        if assembled[idx]:
-            viz_piece( p, ax_cube, color)
-        else: # Individual piece
-            viz_piece( p, ax_pieces[idx], color)
-
     # The individual pieces
     for idx,p in enumerate(pieces):
         color = cmap.colors[idx]
-
-
+        if assembled[idx]:
+            viz_piece_in_cube( p, ax_cube, color)
+        else:
+            viz_piece( p, ax_pieces[idx], color)
 
 
 #-----------------------------
@@ -260,5 +248,27 @@ def viz_piece( p, ax, color):
     p = np.rot90( p, 1, axes=(1,0))
     res = ax.voxels( p, facecolors=color,edgecolors='gray', shade=True)
     return res
+
+#-----------------------------------------
+def viz_piece_in_cube( p, ax_cube, color):
+    # Rotate to the non-intuitive dimension order voxels wants
+    p = np.rot90( p, 1, axes=(2,1))
+    p = np.rot90( p, 1, axes=(1,0))
+
+    S = p.shape[0]
+    trans = np.indices( (S+1,S+1,S+1)) * 0.0
+    trans -= S / 2
+    x,y,z = np.indices( (S+1,S+1,S+1)) + trans
+    res = ax_cube.voxels( x,y,z, p, facecolors=color,edgecolors='gray', shade=True)
+
+    if viz_piece_in_cube.firstcall:
+        viz_piece_in_cube.firstcall = False
+        zlim = ax_cube.get_zlim()
+        scale = 0.93
+        ax_cube.set_zlim( zlim[0] * scale, zlim[1] * scale )
+
+    return res
+
+viz_piece_in_cube.firstcall = True
 
 main()
