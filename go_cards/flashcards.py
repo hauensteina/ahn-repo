6796fb4bd0b3f0d69954c0ac66b08efcb2761d6d
Andreas Hauenstein <hauensteina@gmail.com>
2025-02-12 10:44:45 -0800

    flashcards.py: scale down and add cutting marks

--- go_cards/flashcards.py
+++ go_cards/flashcards.py
@@ -7,6 +7,7 @@ import xml.etree.ElementTree as ET
 import argparse
 
 PAGE_WIDTH, PAGE_HEIGHT = letter  # Letter size: 8.5" x 11"
+SCALE_FACTOR = 0.75
 
 #-------------------------------------------------------------
 def main():
@@ -34,12 +35,13 @@ def main():
         
         generate_flashcards(canv, svg_files_group, i, len(cards))
 
+
     canv.save()
     os.remove('blank.svg')
     print()
     print("Ignore any warnings")
     print(f"Flashcards generated in '{output_pdf}'")
-    
+
 #-----------------------------------------------------------------------------------------
 def generate_flashcards(canv, svg_files, numbering_offset, total_cards):
     """
@@ -51,16 +53,13 @@ def generate_flashcards(canv, svg_files, numbering_offset, total_cards):
     """
 
     get_first_svg = lambda lst: lst[0] if lst else 'blank.svg'
-
-    def draw_cutting_lines(c):
-        c.setDash(5, 3)
-        c.line(PAGE_WIDTH/2, 0, PAGE_WIDTH/2, PAGE_HEIGHT)
-        c.line(0, PAGE_HEIGHT/2, PAGE_WIDTH, PAGE_HEIGHT/2)
             
     # 'double_44/0004_f_2.svg' -> '0004'
     cards = sorted(list(set([ os.path.split(x)[-1].split('_')[0] for x in svg_files ])))
         
     # Arrange front side (first page)
+    draw_cutting_marks(canv)
+    scale_canvas(canv)
     for i, card in enumerate(cards):
         front_dia_1 = get_first_svg([ x for x in svg_files if x.endswith(f"{card}_f_1.svg") ])
         front_dia_2 = get_first_svg([ x for x in svg_files if x.endswith(f"{card}_f_2.svg") ])
@@ -70,10 +69,11 @@ def generate_flashcards(canv, svg_files, numbering_offset, total_cards):
         draw_svg_on_canvas(canv, front_dia_1, 2 * row, col)
         draw_svg_on_canvas(canv, front_dia_2, 2 * row + 1, col, f'{i+numbering_offset+1}/{total_cards}')
         
-    draw_cutting_lines(canv)
     canv.showPage()
 
     # Arrange back side (second page)
+    draw_cutting_marks(canv)
+    scale_canvas(canv)
     for i, card in enumerate(cards):
         try:
             backcard = cards[i+1] if i%2 == 0 else cards[i-1]
@@ -88,11 +88,11 @@ def generate_flashcards(canv, svg_files, numbering_offset, total_cards):
         draw_svg_on_canvas(canv, back_dia_1, 2 * row, col)
         draw_svg_on_canvas(canv, back_dia_2, 2 * row + 1, col)
 
-    draw_cutting_lines(canv)
     canv.showPage()
         
 #-------------------------------------------------------------
 def draw_rect(canvas, rect):
+    """ Useful for layout debugging """
     x, y, width, height = rect
     canvas.setLineWidth(1)
     canvas.setStrokeColorRGB(0, 0, 0)
@@ -125,57 +125,53 @@ def draw_svg_on_canvas(canvas, svg_file, row, col, footer = ''):
     col in (0,1)
     y goes from bottom to top
     """
-    try:
-        if not os.path.exists(svg_file):
-            return
-        svg_width, svg_height = get_svg_dimensions(svg_file)
-        card_height = PAGE_HEIGHT / 2
-        card_width = PAGE_WIDTH / 2
-        hgap = PAGE_WIDTH * 0.05
-        vgap = card_height * 0.05
-        botmarg = card_height * 0.1
-        topmarg = card_height * 0.1
-        diagram_width = (PAGE_WIDTH - 2 * hgap) / 2
-        diagram_height = (card_height - botmarg - topmarg - vgap) / 2
-        x = hgap / 2 + col * (diagram_width + hgap)
-        y = PAGE_HEIGHT - (row+1) * diagram_height - topmarg
-        if row >= 1: y -= vgap
-        if row >= 2: y -= botmarg + topmarg
-        if row >= 3: y -= vgap
-                
-        #rect = (x, y, diagram_width, diagram_height)
-        #draw_rect(canvas, rect)
-        # Scale and center the SVG on the rectangle
-        scale_x = diagram_width / svg_width
-        scale_y = diagram_height / svg_height
-        scale = min(scale_x, scale_y)
-        scale = scale_y
-        
-        dx = (diagram_width - svg_width*scale) / 2
-        dy = (diagram_height - svg_height*scale) / 2
-        
-        # Convert to png
-        #BP()
-        png_file = svg_file.replace('.svg', '.png')
-        
-        convert_svg_to_png_inkscape(svg_file, png_file, width=int(10*svg_width*scale), height=int(10*svg_height*scale))
-        canvas.drawImage(png_file, x + dx, y + dy, width=svg_width*scale, height=svg_height*scale)
-        os.remove(png_file)
-        
-        if footer:
-            # Draw footer
-            font = "Helvetica"
-            size = 10
-            textwidth = canvas.stringWidth(footer, font, size)
-            canvas.setFont(font, size)
-            canvas.drawString(card_width * col + card_width/2 - textwidth/2, 
-                              (3 - row) / 2 * card_height + botmarg / 2, 
-                              footer)
+    if not os.path.exists(svg_file):
+        return
+    svg_width, svg_height = get_svg_dimensions(svg_file)
+    card_height = PAGE_HEIGHT / 2
+    card_width = PAGE_WIDTH / 2
+    hgap = PAGE_WIDTH * 0.05
+    vgap = card_height * 0.05
+    botmarg = card_height * 0.1
+    topmarg = card_height * 0.1
+    diagram_width = (PAGE_WIDTH - 2 * hgap) / 2
+    diagram_height = (card_height - botmarg - topmarg - vgap) / 2
+    x = hgap / 2 + col * (diagram_width + hgap)
+    y = PAGE_HEIGHT - (row+1) * diagram_height - topmarg
+    if row >= 1: y -= vgap
+    if row >= 2: y -= botmarg + topmarg
+    if row >= 3: y -= vgap
+            
+    #rect = (x, y, diagram_width, diagram_height)
+    #draw_rect(canvas, rect)
+    # Scale and center the SVG on the rectangle
+    scale_x = diagram_width / svg_width
+    scale_y = diagram_height / svg_height
+    scale = min(scale_x, scale_y)
+    scale = scale_y
     
-    except:
-        BP()
-        tt=42
-
+    dx = (diagram_width - svg_width*scale) / 2
+    dy = (diagram_height - svg_height*scale) / 2
+    
+    # Convert to png
+    #BP()
+    png_file = svg_file.replace('.svg', '.png')
+    
+    upsample_factor = 3 # reduce pixelation
+    convert_svg_to_png_inkscape(svg_file, png_file, width=int(upsample_factor*svg_width*scale), height=int(upsample_factor*svg_height*scale))
+    canvas.drawImage(png_file, x + dx, y + dy, width=svg_width*scale, height=svg_height*scale)
+    os.remove(png_file)
+    
+    if footer:
+        # Draw footer
+        font = "Helvetica"
+        size = 10
+        textwidth = canvas.stringWidth(footer, font, size)
+        canvas.setFont(font, size)
+        canvas.drawString(card_width * col + card_width/2 - textwidth/2, 
+                            (3 - row) / 2 * card_height + botmarg / 2, 
+                            footer)
+    
 #-------------------------------------------------------------        
 def get_svg_dimensions(svg_file):
     """Extracts width and height from the <svg> tag in an SVG file."""
@@ -190,6 +186,37 @@ def get_svg_dimensions(svg_file):
 def generate_blank_svg(fname): 
     with open(fname, 'w') as f:
         f.write('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" height="100"></svg>')
+  
+#---------------------------------------------------------------------------
+def scale_canvas(c):
+    """Scale and center canvas"""
+    scaled_width = PAGE_WIDTH * SCALE_FACTOR
+    scaled_height = PAGE_HEIGHT * SCALE_FACTOR
+    translate_x = (PAGE_WIDTH - scaled_width) / 2
+    translate_y = (PAGE_HEIGHT - scaled_height) / 2
+    c.translate(translate_x, translate_y)  # Move origin to center
+    c.scale(SCALE_FACTOR, SCALE_FACTOR)    # Scale content
 
+#-------------------------------------------------------------
+def draw_cutting_marks(c):
+    hmarg = (PAGE_HEIGHT - PAGE_HEIGHT * SCALE_FACTOR) / 2
+    vmarg = (PAGE_WIDTH - PAGE_WIDTH * SCALE_FACTOR) / 2
+    c.setLineWidth(0.5)
+    # marks for the vertical cut
+    c.line(hmarg, 0, hmarg, 10)
+    c.line(PAGE_WIDTH - hmarg, 0, PAGE_WIDTH - hmarg, 10)
+    c.line(hmarg, PAGE_HEIGHT, hmarg, PAGE_HEIGHT - 10)
+    c.line(PAGE_WIDTH - hmarg, PAGE_HEIGHT, PAGE_WIDTH - hmarg, PAGE_HEIGHT - 10)
+    # marks for the horizontal cut
+    c.line(0, vmarg, 10, vmarg)
+    c.line(0, PAGE_HEIGHT - vmarg, 10, PAGE_HEIGHT - vmarg)
+    c.line(PAGE_WIDTH, vmarg, PAGE_WIDTH - 10, vmarg)
+    c.line(PAGE_WIDTH, PAGE_HEIGHT - vmarg, PAGE_WIDTH - 10, PAGE_HEIGHT - vmarg)
+    # A cross in the center
+    c.line(PAGE_WIDTH/2 - 5, PAGE_HEIGHT/2, PAGE_WIDTH/2 + 5, PAGE_HEIGHT/2)
+    c.line(PAGE_WIDTH/2, PAGE_HEIGHT/2 - 5, PAGE_WIDTH/2, PAGE_HEIGHT/2 + 5)
+    
+    
+    
         
 main()
