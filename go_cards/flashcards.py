e8e769638df0b976181ed82389639d04d69a8a6b
Andreas Hauenstein <hauensteina@gmail.com>
2025-02-12 08:58:27 -0800

    flashcards.py: topmarg and botmarg

--- go_cards/flashcards.py
+++ go_cards/flashcards.py
@@ -16,19 +16,23 @@ def main():
     
     generate_blank_svg('blank.svg')
     svg_files = [f'{args.folder}/{f}' for f in os.listdir(args.folder) if f.endswith('.svg')]
-    # Pad with blank cards to make the number of cards a multiple of 4
-    while len(svg_files) % 4 != 0:
-        svg_files.append('blank.svg')
     output_pdf = f'{args.folder}/{args.folder}_flashcards.pdf'
-    canv= canvas.Canvas(output_pdf, pagesize=letter)
+    canv = canvas.Canvas(output_pdf, pagesize=letter)
     
     cards = sorted(list(set([ os.path.split(x)[-1].split('_')[0] for x in svg_files ])))
+    
     # Go through cards in groups of 4
     for i in range(0, len(cards), 4):
         print(f"Processing cards {i+1} to {i+4}")
-        cards_group = cards[i:i+4]
+
+        # Four cards make a page
+        if i+4 > len(cards):
+            cards_group = cards[i:]
+        else:
+            cards_group = cards[i:i+4] 
         svg_files_group = [f for f in svg_files if f'{os.path.split(f)[-1].split("_")[0]}' in cards_group]      
-        generate_flashcards(canv, svg_files_group, output_pdf)
+        
+        generate_flashcards(canv, svg_files_group, i, len(cards))
 
     canv.save()
     os.remove('blank.svg')
@@ -36,11 +40,17 @@ def main():
     print("Ignore any warnings")
     print(f"Flashcards generated in '{output_pdf}'")
     
-#-------------------------------------------------------------
-def generate_flashcards(canv, svg_files, output_pdf):
-    """Generate a two-page PDF with the flashcards arranged for duplex printing."""
+#-----------------------------------------------------------------------------------------
+def generate_flashcards(canv, svg_files, numbering_offset, total_cards):
+    """
+        Draw two pages on a canvas.
+        Four cards fit on a duplex sheet.
+        The first page has the front side of the cards.
+        The second page has the back side of the cards.
+        Each side of a card has one or two diagrams.    
+    """
 
-    get_first = lambda lst: lst[0] if lst else ''
+    get_first_svg = lambda lst: lst[0] if lst else 'blank.svg'
 
     def draw_cutting_lines(c):
         c.setDash(5, 3)
@@ -49,35 +59,43 @@ def generate_flashcards(canv, svg_files, output_pdf):
             
     # 'double_44/0004_f_2.svg' -> '0004'
     cards = sorted(list(set([ os.path.split(x)[-1].split('_')[0] for x in svg_files ])))
-    # Swap the order of each pair in cards
-    backs = [ cards[i+1] if i % 2 == 0 else cards[i-1] for i in range(len(cards)) ]
         
     # Arrange front side (first page)
     for i, card in enumerate(cards):
-        front_1 = get_first([ x for x in svg_files if x.endswith(f"{card}_f_1.svg") ])
-        front_2 = get_first([ x for x in svg_files if x.endswith(f"{card}_f_2.svg") ])
+        front_dia_1 = get_first_svg([ x for x in svg_files if x.endswith(f"{card}_f_1.svg") ])
+        front_dia_2 = get_first_svg([ x for x in svg_files if x.endswith(f"{card}_f_2.svg") ])
         row = i // 2
         col = i % 2
 
-        draw_svg_on_canvas(canv, front_1, 2 * row, col)
-        draw_svg_on_canvas(canv, front_2, 2 * row + 1, col)
-
+        draw_svg_on_canvas(canv, front_dia_1, 2 * row, col)
+        draw_svg_on_canvas(canv, front_dia_2, 2 * row + 1, col)
+        k = i + 1 + numbering_offset
+        draw_numbering(canv, f'{k}/{total_cards}')
+        
     draw_cutting_lines(canv)
     canv.showPage()
 
     # Arrange back side (second page)
-    for i, card in enumerate(backs):
-        back_1 = get_first([ x for x in svg_files if x.endswith(f"{card}_b_1.svg") ])
-        back_2 = get_first([ x for x in svg_files if x.endswith(f"{card}_b_2.svg") ])
+    for i, card in enumerate(cards):
+        try:
+            backcard = cards[i+1] if i%2 == 0 else cards[i-1]
+        except:
+            backcard = 'xxx'
+        back_dia_1 = get_first_svg([ x for x in svg_files if x.endswith(f"{card}_b_1.svg") ])
+        back_dia_2 = get_first_svg([ x for x in svg_files if x.endswith(f"{card}_b_2.svg") ])
 
         row = i // 2
         col = i % 2
 
-        draw_svg_on_canvas(canv, back_1, 2 * row, col)
-        draw_svg_on_canvas(canv, back_2, 2 * row + 1, col)
+        draw_svg_on_canvas(canv, back_dia_1, 2 * row, col)
+        draw_svg_on_canvas(canv, back_dia_2, 2 * row + 1, col)
 
     draw_cutting_lines(canv)
     canv.showPage()
+    
+#-------------------------------------------------------------    
+def draw_numbering(canvas, text):
+    pass    
     
 #-------------------------------------------------------------
 def draw_rect(canvas, rect):
@@ -107,35 +125,51 @@ def convert_svg_to_png_inkscape(svg_file, png_file, width, height):
     
 #-------------------------------------------------------------
 def draw_svg_on_canvas(canvas, svg_file, row, col):
-    """Draw an SVG file on the ReportLab canvas using svglib."""
-    if not os.path.exists(svg_file):
-        return
-    svg_width, svg_height = get_svg_dimensions(svg_file)
-    hgap = page_width * 0.05
-    vgap = page_height * 0.05
-    diagram_width = (page_width - 2 * hgap) / 2
-    diagram_height = (page_height - 4 * vgap) / 4
-    x = hgap / 2 + col * (diagram_width + hgap)
-    y = page_height - (row+1) * diagram_height - vgap / 2 - row * vgap
-    #rect = (x, y, diagram_width, diagram_height)
-    #draw_rect(canvas, rect)
-
-    # Scale and center the SVG on the rectangle
-    scale_x = diagram_width / svg_width
-    scale_y = diagram_height / svg_height
-    scale = min(scale_x, scale_y)
-    scale = scale_y
+    """
+    Draw an SVG file on the ReportLab canvas using svglib.
+    row in (0,1,2,3) 
+    col in (0,1)
+    y goes from bottom to top
+    """
+    try:
+        if not os.path.exists(svg_file):
+            return
+        svg_width, svg_height = get_svg_dimensions(svg_file)
+        card_height = page_height / 2
+        hgap = page_width * 0.05
+        vgap = card_height * 0.05
+        botmarg = card_height * 0.1
+        topmarg = card_height * 0.1
+        diagram_width = (page_width - 2 * hgap) / 2
+        diagram_height = (card_height - botmarg - topmarg - vgap) / 2
+        x = hgap / 2 + col * (diagram_width + hgap)
+        y = page_height - (row+1) * diagram_height - topmarg
+        if row >= 1: y -= vgap
+        if row >= 2: y -= botmarg + topmarg
+        if row >= 3: y -= vgap
+                
+        #rect = (x, y, diagram_width, diagram_height)
+        #draw_rect(canvas, rect)
+        # Scale and center the SVG on the rectangle
+        scale_x = diagram_width / svg_width
+        scale_y = diagram_height / svg_height
+        scale = min(scale_x, scale_y)
+        scale = scale_y
+        
+        dx = (diagram_width - svg_width*scale) / 2
+        dy = (diagram_height - svg_height*scale) / 2
+        
+        # Convert to png
+        #BP()
+        png_file = svg_file.replace('.svg', '.png')
+        
+        convert_svg_to_png_inkscape(svg_file, png_file, width=int(10*svg_width*scale), height=int(10*svg_height*scale))
+        canvas.drawImage(png_file, x + dx, y + dy, width=svg_width*scale, height=svg_height*scale)
+        os.remove(png_file)
     
-    dx = (diagram_width - svg_width*scale) / 2
-    dy = (diagram_height - svg_height*scale) / 2
-    
-    # Convert to png
-    png_file = svg_file.replace('.svg', '.png')
-    
-    convert_svg_to_png_inkscape(svg_file, png_file, width=int(10*svg_width*scale), height=int(10*svg_height*scale))
-    
-    canvas.drawImage(png_file, x + dx, y + dy, width=svg_width*scale, height=svg_height*scale)
-    os.remove(png_file)
+    except:
+        BP()
+        tt=42
 
 #-------------------------------------------------------------        
 def get_svg_dimensions(svg_file):
