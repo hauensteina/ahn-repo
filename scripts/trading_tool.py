83ecb041e53108aca6c1701b63f2e1eb11eaab53
Andreas Hauenstein <hauensteina@gmail.com>
2025-06-09 12:45:36 -0700

    trading_tool.py: Add simulation mode

--- scripts/trading_tool.py
+++ scripts/trading_tool.py
@@ -87,13 +87,12 @@ import yfinance as yf
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import time
 from io import StringIO 
 import copy
 from zoneinfo import ZoneInfo
 import pandas_market_calendars as mcal
-from datetime import datetime
 
 from pdb import set_trace as BP
 
@@ -114,24 +113,80 @@ def usage():
         
         Each investor holds n_stocks different tickers. Each day, we find the best (according to strategy) n_stocks and buy them.
         Stocks not in the top n_stocks are sold. 
+        
+    Options:
+        --run: Run the trading simulation every day at noon New York time, starting today.
+        --simulate: Use data from the past year to simulate a year of trading
 
     Synopsis:
-      {name} --run
+      {name} --mode [ run | simulate ]
 
-    Example:
-      python {name} --run
-
+    Examples:
+        python {name} --mode run
+        python {name} --mode simulate
+    
 ''' 
     msg += '\n '
     return msg 
 
 #-----------------------
 def main():
-    
     parser = argparse.ArgumentParser( usage=usage())
-    parser.add_argument('--run', action='store_true', help='Run the trading simulation', required=True)
+    parser.add_argument('--mode', choices=['run', 'simulate'], default='simulate', help='Mode of operation')
     args = parser.parse_args()
+
+    if args.mode == 'run':
+        print("Running the trading simulation...")
+        run()
+    else: 
+        print("Trading with last year's data...")
+        simulate()
+        
+#--------------------------------------------------------        
+def simulate():
+    """
+    Simulate trading with historical data for the past year.
+    This function will read the initial state from trading_tool_investors.json,
+    fetch historical data for the past year, and simulate trades based on the defined strategies.
+    """
     
+    # Load initial state
+    state = json.load(open('trading_tool_investors.json', 'r'))
+    
+    # Set start and end dates for simulation
+    end_date = datetime.now(ZoneInfo("America/New_York"))
+    start_date = end_date - timedelta(days=365)  # One year of data
+    
+    tickers = get_sp500_tickers()
+    #metadata = get_sp500_metadata()
+    # See if we have a pickle file with the data
+    #close_price_data, volume_data = get_data(tickers, start_date, end_date)
+    try:
+        pickle = "sp500_year_ending_mon_2025-06-09.pkl"
+        with open(pickle, 'rb') as f: data = pd.read_pickle(f)
+        close_price_data = data['Close']
+        volume_data = data['Volume']
+        print(f"Loaded ticker data from file: {pickle}")
+        end_date = datetime.fromisoformat('2025-06-09')
+        start_date = end_date - timedelta(days=365)
+    except FileNotFoundError:
+        print(f"Getting ticker data from yfinance")
+        close_price_data, volume_data = get_data(tickers, start_date, end_date)
+
+    days = close_price_data.index[30:]
+    for idx,day in enumerate(days):
+        print(f"\nDay {idx+1}: {day.strftime('%Y-%m-%d')}")
+        print('===============================================================')
+        start_date = day - timedelta(days=30)  # one month of data
+        close_30_days = close_price_data.loc[start_date:day]
+        volume_30_days = volume_data.loc[start_date:day]
+        for investor in state['investors']:
+            # Simulate trading for each day
+            trade_portfolio(investor, close_30_days, volume_30_days)
+        print_portfolio_values(state['investors'], close_30_days)
+
+#-----------------------------------------------------------------------
+def run():
     # Sleep and check occasionally whether it is 9 am and the stock market is open
     while True:
         while True: # Make a block to break out of
@@ -150,16 +205,17 @@ def main():
         time.sleep(30)
         
 # sort_stocks() defines the way we pick stocks.  
-#------------------------------------------------------------------------------------
-def sort_stocks(ticker_data, N_STOCKS, criterion, long_start_date, n_days_spike):
+# close_price_data and volume_data cover the previous 30 days.
+#-----------------------------------------------------------------------------------------
+def sort_stocks(close_price_data, volume_data, criterion, n_days_spike):
     """
     Sort tickers based on recent changes in price and/or volume.
     """
 
-    avg_volume_by_ticker = ticker_data['Volume'].mean()
-    recent_volume_by_ticker = ticker_data['Volume'].iloc[-1]
+    avg_volume_by_ticker = volume_data.mean()
+    recent_volume_by_ticker = volume_data.iloc[-1]
     volrat_by_ticker = recent_volume_by_ticker / avg_volume_by_ticker # yesterdays volume over avg 30 day volume
-    recent_price_increase_by_ticker = ticker_data['Close'].iloc[-1] / ticker_data['Close'].iloc[-n_days_spike] # yesterdays price over price n days ago
+    recent_price_increase_by_ticker = close_price_data.iloc[-1] / close_price_data.iloc[-n_days_spike] # yesterdays price over price n days ago
 
     if criterion == 'incr':
         crit_by_ticker = recent_price_increase_by_ticker
@@ -203,13 +259,13 @@ def trade(state):
     LONG_START_DATE = END_DATE - timedelta(days=N_DAYS_LONG)
 
     tickers = get_sp500_tickers()
-    ticker_data = get_data(tickers, LONG_START_DATE, END_DATE)
+    close_price_data, volume_data = get_data(tickers, LONG_START_DATE, END_DATE)
     metadata = get_sp500_metadata()
 
     for investor in investors:
-        trade_portfolio(investor, ticker_data, metadata, LONG_START_DATE)
+        trade_portfolio(investor, close_price_data, volume_data)
 
-    print_portfolio_values(investors, ticker_data)
+    print_portfolio_values(investors, close_price_data)
     print()
 
     # Save the updated investors data back to the file
@@ -222,13 +278,14 @@ def today():
     """ Returns current New York date in 'YYYY-MM-DD' format """
     return datetime.now(ZoneInfo("America/New_York")).strftime('%Y-%m-%d')
 
-#-----------------------------------------------------------------------
-def trade_portfolio(investor, ticker_data, metadata, long_start_date):
+#------------------------------------------------------------------------------------
+def trade_portfolio(investor, close_price_data, volume_data):
     print( '\nTrading for Investor: ', investor['name'])
+    print('--------------------------------------------------')
     criterion = investor['parameters']['criterion']
     n_stocks = investor['parameters']['n_stocks']
     n_days_spike = investor['parameters']['n_days_spike']
-    top_stocks = dict(list(sort_stocks(ticker_data, n_stocks, criterion, long_start_date, n_days_spike).items())[:n_stocks])
+    top_stocks = dict(list(sort_stocks(close_price_data, volume_data, criterion, n_days_spike).items())[:n_stocks])
     # Sell any stocks that are not in the top N
     my_stocks = investor['portfolio_log'][-1]['stocks']
     # Get a copy of the last log entry
@@ -240,28 +297,29 @@ def trade_portfolio(investor, ticker_data, metadata, long_start_date):
     for ticker in my_stocks:
         if ticker not in top_stocks:
             print(f"Selling {ticker} from {investor['name']}'s portfolio")
-            sell_ticker(investor, ticker, ticker_data, new_log_entry, last_log_entry)
+            sell_ticker(investor, ticker, close_price_data, new_log_entry, last_log_entry)
 
     # Buy equal amounts of the top N stocks we don't already have
     stocks_to_buy = [ticker for ticker in top_stocks if ticker not in my_stocks]
-    print(f"Buying {len(stocks_to_buy)} stocks for {investor['name']}: {', '.join(stocks_to_buy)}")
+    print(f"\nBuying {len(stocks_to_buy)} stocks for {investor['name']}: {', '.join(stocks_to_buy)}")
     cash = new_log_entry['cash']
     # Calculate equal investment amount for each stock
     investment_per_stock = cash / len(stocks_to_buy)
     for ticker in stocks_to_buy:
-        buy_ticker(investor, ticker, investment_per_stock, ticker_data, new_log_entry)
-        
+        buy_ticker(investor, ticker, investment_per_stock, close_price_data, new_log_entry)
+
+    print()
     # Update the portfolio log with the new log entry
     investor['portfolio_log'].append(new_log_entry)
     
 #-----------------------------------------------------------------------------------
-def buy_ticker(investor, ticker, investment_amount, ticker_data, new_log_entry):
+def buy_ticker(investor, ticker, investment_amount, close_price_data, new_log_entry):
     """
     Buy a ticker for the investor's portfolio.
     This function should update the portfolio_log with the purchase details.
     """
     # Get the last price of the ticker
-    last_price = ticker_data['Close'].iloc[-1][ticker]
+    last_price = close_price_data.iloc[-1][ticker]
     # Calculate number of shares to buy
     shares = int(investment_amount / last_price)
 
@@ -281,13 +339,13 @@ def buy_ticker(investor, ticker, investment_amount, ticker_data, new_log_entry):
     print(f"Bought {shares} shares of {ticker} at {last_price:.2f}, remaining cash: {new_log_entry['cash']:.2f}")
 
 #--------------------------------------------------------------------------------
-def sell_ticker(investor, ticker, ticker_data, new_log_entry, old_log_entry):
+def sell_ticker(investor, ticker, close_price_data, new_log_entry, old_log_entry):
     """
     Sell a ticker from the investor's portfolio.
     This function should update the portfolio_log with the sale details.
     """
     # Get the last price of the ticker
-    last_price = ticker_data['Close'].iloc[-1][ticker]
+    last_price = close_price_data.iloc[-1][ticker]
     # Update portfolio log
     shares = new_log_entry['stocks'][ticker]['shares']
     cash = new_log_entry['cash'] + (shares * last_price)
@@ -301,7 +359,7 @@ def sell_ticker(investor, ticker, ticker_data, new_log_entry, old_log_entry):
     print(f"Sold {shares} shares of {ticker} at {last_price:.2f}, new cash balance: {cash:.2f}")
     
 #-------------------------------------------------------
-def print_portfolio_values(investors, ticker_data):
+def print_portfolio_values(investors, close_price_data):
     """
     Print the current value of each investor's portfolio.
     """
@@ -314,7 +372,7 @@ def print_portfolio_values(investors, ticker_data):
             shares = stock_info['shares']
             price_bought = stock_info['price_bought']
             price_sold = stock_info.get('price_sold', None)
-            current_price = ticker_data['Close'].iloc[-1][ticker]
+            current_price = close_price_data.iloc[-1][ticker]
             value = shares * current_price
             total_value += value
             print(f"{ticker}: {shares} shares bought at {price_bought:.2f}, current price: {current_price:.2f}, value: {value:.2f}")
@@ -355,8 +413,6 @@ def get_sp500_metadata():
     }
     return metadata_dict
 
-
-
 #---------------------------------------------------------------------------------
 def get_data(tickers, start_date, end_date):
     """
@@ -364,13 +420,16 @@ def get_data(tickers, start_date, end_date):
     """
     # yfinance handles up to 200 tickers per call, so split
     batches = [tickers[i:i + 100] for i in range(0, len(tickers), 100)]
-    price_volume_data = pd.DataFrame()
+    close_price_data = pd.DataFrame()
+    volume_data = pd.DataFrame()
     for batch in batches:
         df = fetch_ticker_data(batch, start_date, end_date)
-        price_volume_data = pd.concat([price_volume_data, df], axis=1)
+        close_price = df['Close']
+        volume = df['Volume']
+        close_price_data = pd.concat([close_price_data, close_price], axis=1)
+        volume_data = pd.concat([volume_data, volume], axis=1)
 
-    res = price_volume_data.dropna(axis=1) # Drop tickers with all-NaN data
-    return res
+    return close_price_data, volume_data
 
 #------------------------------------------------------
 def fetch_ticker_data(tickers, start_date, end_date):
@@ -386,7 +445,6 @@ def fetch_ticker_data(tickers, start_date, end_date):
 #----------------------------
 def clean_ticker(ticker):
     return ticker.replace('.', '-')
-
 
 if __name__ == '__main__':
     #trade() # test run
