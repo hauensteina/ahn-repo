7f565a9a46d85149479d71bc8143d59b4b80900e
Andreas Hauenstein <hauensteina@gmail.com>
2025-06-25 16:07:30 -0700

    browsecards.py auto-generates an index page

--- go_cards/browsecards.py
+++ go_cards/browsecards.py
@@ -5,71 +5,146 @@ import http.server
 import json
 import os
 import shutil
-import socketserver
-class ReusableTCPServer(socketserver.TCPServer):
-    allow_reuse_address = True
 import tempfile
+import json
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 import webbrowser
+import threading
+import time
+
+#==========================================================
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            html = get_index_html()
+            self.wfile.write(html.encode('utf-8'))
+        elif '.svg' in self.path:
+            # Cut off leading '/folder/' if it exists
+            if self.path.startswith("/folder/"):
+                self.path = self.path.split("/folder/")[-1]
+            # serve the SVG file 
+            try:
+                with open(self.path, 'rb') as f:
+                    self.send_response(200)
+                    self.send_header("Content-type", "image/svg+xml")
+                    self.end_headers()
+                    self.wfile.write(f.read())
+            except FileNotFoundError:
+                self.send_error(404, "File not found")
+        elif self.path.startswith("/folder/"):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            folder = self.path.split("/folder/")[-1]
+            print(">>>>>>>>>>>>>>>>>>>>>>>>", folder)
+            html = get_folder_html(folder)
+            self.wfile.write(html.encode('utf-8'))
+        else:
+            self.send_error(404, "Folder not found")    
 
 # -----------------
 def main():
     parser = argparse.ArgumentParser(
         description='Browse through svg flashcards in a folder')
     parser.add_argument('--folder', type=str,
-                        help='folder containing the SVG files', required=True)
+                        help='folder containing the SVG files')
     args = parser.parse_args()
 
-    blank_svg = f'{args.folder}/blank.svg'
-    generate_blank_svg(blank_svg)
+# -----------------------------
+def get_index_html():
+    """
+    Get a list of all folders with files like 0001_f_1.svg in it and 
+    present an html page with list of links to those folders. Clicking on a link will 
+    send a request to the server to 
+    """
+    folders = []
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith('_f_1.svg'):
+                folder = os.path.relpath(root, '.')
+                if folder not in folders:
+                    folders.append(folder)
+    folders = sorted(folders)
+    html = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>Flashcard Browser</title>
+            <style>
+            body { text-align: center; font-family: sans-serif; font-size: 2em;}
+            a, a:visited { display: block; margin: 1%%; text-decoration: none; color: #0000ee;}
+            a:hover { color: #0000ee; text-decoration:underline; }
+  
+        </style>
+        </head>
+        <body>
+        <h1>Flashcard Browser</h1>
+        %s
+        </body>
+        </html>
+    """
+    links = []
+    for folder in folders:
+        folder_name = os.path.split(folder)[-1]
+        title = folder_name.replace("_", " ").title()
+        link = f'<a href="folder/{folder}">{title}</a>'
+        links.append(link)
+    links_html = '\n'.join(links)
+    html = html % links_html    
+    return html
+        
+
+# ---------------------------------
+def get_folder_html( folder):
+    print(f'>>>>>>>{folder}')
+    BLANK_SVG = 'blank.svg'
+    generate_blank_svg(BLANK_SVG)
 
     svg_files = [
-        f'{args.folder}/{f}' for f in os.listdir(args.folder) if f.endswith('.svg')]
+        f'{folder}/{f}' for f in os.listdir(folder) if f.endswith('.svg')]
     cards = sorted(
         list(set([os.path.split(x)[-1].split('_')[0] for x in svg_files])))
     cards = [x for x in cards if not 'blank' in x and not '0000' in x and not 'title' in x ]
-    print(cards)
 
     svg_pairs = []
     for idx, card in enumerate(cards):
         svgs = []
         captions = []
         for dia in ['f_1', 'f_2', 'b_1', 'b_2']:
-            fname = f"{args.folder}/{card}_{dia}.svg"
-            svg = fname if os.path.exists(fname) else blank_svg
+            fname = f"{folder}/{card}_{dia}.svg"
+            svg = fname if os.path.exists(fname) else BLANK_SVG
             caption = get_caption(open(svg, 'r').read())
             captions.append(caption)
             svgs.append(svg)
 
         front_1, front_2 = (svgs[0], captions[0]), (svgs[1], captions[1])
         back_1, back_2 = (svgs[2], captions[2]), (svgs[3], captions[3])
-        if back_2[0] == blank_svg:
+        if back_2[0] == BLANK_SVG:
             back_1, back_2 = back_2, back_1
 
         svg_pairs.append((front_1, front_2))
         svg_pairs.append((back_1, back_2))
-
-    htmldir = os.path.join(args.folder, "html")
-    os.makedirs(htmldir, exist_ok=True)
-    indexhtml = generate_html_files(svg_pairs, htmldir)
-    serve_html(htmldir, indexhtml)
-
-# -------------------------------------------------------------
-def generate_blank_svg(fname):
-    with open(fname, 'w') as f:
-        f.write(
-            '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" height="100"></svg>')
+    
+    html = generate_folder_html(svg_pairs, folder)
+    return html
 
 # ----------------------------------------------
-def generate_html_files(svg_pairs, folder):
-    title = os.path.split(folder)[-2]
-    svg_pairs_base = [(os.path.basename(pair[0][0]),
-                       os.path.basename(pair[1][0])) for pair in svg_pairs]
+def generate_folder_html(svg_pairs, folder):
+    title = folder
+    svg_pairs_base = [(pair[0][0], pair[1][0]) for pair in svg_pairs]
     svg_pairs_base_json = json.dumps(svg_pairs_base)
     caption_pairs = [(pair[0][1], pair[1][1]) for pair in svg_pairs]
     caption_pairs = json.dumps(caption_pairs)
+
     cards = [ x[0] if not 'blank' in x[0] else x[1] for x in svg_pairs_base ]
+    cards = [ x.split('/')[-1] for x in cards ]
     cards = [ x.split('_')[0] for x in cards ]
-    viewer = os.path.join(folder, "index.html")
+    #viewer = os.path.join(folder, "index.html")
     html = """
         <!DOCTYPE html>
         <html>
@@ -81,14 +156,15 @@ def generate_html_files(svg_pairs, folder):
             figure { display: inline-block; margin: 1%%; }
             img { width: 45vw; height: auto; border: none; }
             figcaption { margin-top: 0.5em; color: #444; }
-        </style>
+            a, a:visited { text-decoration: none; color: #0000ee }
+            a:hover { color: #0000ee; text-decoration:underline; }
+          </style>
         </head>
         <body>
-        <div id="heading">
+        <div id="heading"  style="text-align: left; margin-top: 30px; margin-left:120px ">
             <bold>%s</bold>
         </div>
         <div id="image-container">
-        <img src="0020_f_1.svg"><img src="0020_f_2.svg">
         </div>
         <script>
             const svgPairs = %s;
@@ -100,20 +176,24 @@ def generate_html_files(svg_pairs, folder):
             let nprobs = svgPairs.length / 2;
 
             function render() {
-		    const pair = svgPairs[index];
-		    const captionPair = captionPairs[index];
-                    const card = cards[index];
-		    document.getElementById("image-container").innerHTML = `
-			<figure>
-			<img src="${pair[0]}">
-			<figcaption>${captionPair[0]}</figcaption>
-			</figure>
-			<figure>
-			<img src="${pair[1]}">
-			<figcaption>${captionPair[1]}</figcaption>
-			</figure>`;
-		    document.getElementById("heading").innerHTML = `
-			<bold>${title} ${probnum}/${nprobs}</bold> (files ${card}_)`;
+                const pair = svgPairs[index];
+                const captionPair = captionPairs[index];
+                        const card = cards[index];
+                document.getElementById("image-container").innerHTML = `
+                <figure>
+                <img src="${pair[0]}">
+                <figcaption>${captionPair[0]}</figcaption>
+                </figure>
+                <figure>
+                <img src="${pair[1]}">
+                <figcaption>${captionPair[1]}</figcaption>
+                </figure>`;
+                document.getElementById("heading").innerHTML = `
+                <span>
+                <a href="/">Back</a>
+                &nbsp;
+                <bold>${title} ${probnum}/${nprobs}</bold> (files ${card}_)
+                </span>`;
             } // render()
 
             document.addEventListener("keydown", (e) => {
@@ -151,33 +231,7 @@ def generate_html_files(svg_pairs, folder):
         </body>
         </html>    
     """ % (title, svg_pairs_base_json, cards, caption_pairs, title)
-    with open(viewer, "w") as f:
-        f.write(html)
-
-    # Copy the SVG files into the temp folder
-    for pair in svg_pairs:
-        for svg in pair:
-            shutil.copy(svg[0], os.path.join(folder, os.path.basename(svg[0])))
-
-    return viewer
-
-# ----------------------------------------------
-def serve_html(folder, html_file):
-    pwd = os.getcwd()
-    os.chdir(folder)
-    port = 8000
-    handler = http.server.SimpleHTTPRequestHandler
-    with ReusableTCPServer(("", port), handler) as httpd:
-        webbrowser.open(
-            f"http://localhost:{port}/{os.path.basename(html_file)}")
-        print(f"Serving at http://localhost:{port}/")
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            print("Server stopped.")
-            os.chdir(pwd)
-            shutil.rmtree(folder)
-            print("Temporary html folder removed.")
+    return html
 
 # -------------------------------------------------------------
 def get_caption(txt):
@@ -191,6 +245,17 @@ def get_caption(txt):
     res = txt[start+9:end].strip()
     return res
 
+# -------------------------------------------------------------
+def generate_blank_svg(fname):
+    with open(fname, 'w') as f:
+        f.write(
+            '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" height="100"></svg>')
 
+# -------------------------------------------------------------
 if __name__ == "__main__":
-    main()
+    #threading.Thread(target=start_server, daemon=True).start()
+    #time.sleep(1)  # Give the server a moment to start
+    server = HTTPServer(("localhost", 8000), MyHandler)
+    webbrowser.open("http://localhost:8000/")
+    print("Server running at http://localhost:8000")
+    server.serve_forever()
